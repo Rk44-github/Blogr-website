2f498be6f713f9fa69e1023551e25f473f4eae16
Raj kumar <rajuttam4447@gmail.com>
2022-03-22 17:12:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// var hasClass = '' ;
+// const menu = document.getElementsByClassName('burger');
+
+
+// menu[0].addEventListener('click', function(){
+//     nav.add('reveal');
+//     console.log(nav);
+    
+    
+// });
+// nav.forEach(removeClass);
+
+// function removeClass(){
+//    nav.addEventListener('click', ()=> {
+//        nav.remove('reveal')
+//     });
+   
+// }
+$(document).ready(function(){
+       
+   const navBar = $('.nav-list');
+   const nav = 
+        
+       $('.burger').click( ()=> {
+         
+                   navBar.toggleClass("reveal");
+       })
+  
+
+});
+   
+
